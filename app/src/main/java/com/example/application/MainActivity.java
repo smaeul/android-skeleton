e114b637309c27b79645a727fe45c3b13db8d669
Samuel Holland <samuel@sholland.org>
2017-09-14 18:09:14 -0500

    MainActivity: Create

---
+++ app/src/main/java/com/example/application/MainActivity.java
@@ -0,0 +1,11 @@
+package com.example.application;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+    @Override
+    protected void onCreate(final Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
